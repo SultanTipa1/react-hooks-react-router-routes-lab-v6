f788d92bbb8d4d21ef89bd6bca986655f613b60c
SultanTipa1 <josephngumbaugush@gmail.com>
2024-08-15 21:17:18 +0300

    Adds all files

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,4 +1,9 @@
-import { useEffect, useState } from "react";
+import { createBrowserRouter } from "react-router-dom";
+// import { useEffect, useState } from "react";
+import Home from "./components/Home";
+import Actors from "./components/Actors";
+import Directors from "./components/Directors";
+import Movie from "./components/Movie";
 
 function Movie() {
   return (
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,30 @@
-
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home";
+import Actors from "./components/Actors";
+import Directors from "./components/Directors";
+import Movie from "./components/Movie";
 
 const routes = [
-   
-  ];
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/actors",
+    element: <Actors />,
+  },
+  {
+    path: "/directors",
+    element: <Directors />,
+  },
+  {
+    path: "/movie/:id",
+    element: <Movie />,
+  },
+  {
+    path: "*",
+    element: <div>404 Not Found</div>,
+  },
+];
 
 export default routes;
